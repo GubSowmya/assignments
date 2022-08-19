5204520137d4e246fb9a81928e36e0c020a1ab0b
John Max <example@simplilearn.com>
2022-08-19 10:14:14 +0530

    firstpush

--- Program.cs
+++ Program.cs
@@ -10,20 +10,13 @@ namespace assignments
     {
         static void Main(string[] args)
         {
-            int num1, num2, num3;
-            Console.WriteLine("Input the first number to multiply: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Input the second to multiply :");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Input the third to multiply :");
-            num3 = Convert.ToInt32(Console.ReadLine());
-
-            int result = num1 * num2 * num3;
-            Console.WriteLine("Output : {0} * {1} * {2} = {3}", num1, num2, num3, result);
-            Console.ReadLine();
-
+            int num1=10;
+            int num2 = 20;
+            if (num1 > num2)
+            {
+                Console.WriteLine("10 is small");
+            }
+     
             
 
 
