b19cdcb05c27ca4793076e70a831d04579f69c1a
John Max <example@simplilearn.com>
2022-08-19 09:41:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Input the first number to multiply: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the second to multiply :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the third to multiply :");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 * num2 * num3;
+            Console.WriteLine("Output : {0} * {1} * {2} = {3}", num1, num2, num3, result);
+            Console.ReadLine();
+
+            program-3
+
+
+
+
+        }
+    }
+}
